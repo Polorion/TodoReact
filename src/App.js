cf036ee96761c9c3c7937c3054bbcdb05fa92149
Polorion <97245263+Polorion@users.noreply.github.com>
2022-06-24 15:34:20 +0300

    second

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-import React from "react";
-import StartAppComponent from "./StartAppComponent/StartAppComponent";
-
-function App() {
-  return <StartAppComponent />;
-}
-
-export default App;
--- src/util/generateRandomString.js
+++
@@ -1,3 +0,0 @@
-const GeneratorRandomString = () => Math.random().toString(36).substring(2, 20);
-
-export default GeneratorRandomString;
